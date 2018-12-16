c4f82043c4a517b6ad4ffb328f5bad0f3d3fa871
Pierce C Lauring <pclauring@gmail.com>
2018-12-16 13:53:25 -0500

    card component add

---
+++ ClientApp/src/components/Card.js
@@ -0,0 +1,13 @@
+const Card = () => {
+	return (
+  	<div>
+  	  <img src="http://placehold.it/75" />
+      <div>
+        <div>Name Here ...</div>
+      <div>Company Name Here ...</div>
+      </div>
+  	</div>
+  );
+};
+
+ReactDOM.render(<Card />,mountNode);
